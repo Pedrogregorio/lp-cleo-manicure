1548c1e89fdb2a17f5534aa3e83cee75e7917477
Pedrogregorio <pedrogregoriodias675@gmail.com>
2020-10-29 23:56:54 -0300

    inclusao da pagina de whatsapp

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const consign = require('consign')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express()
 
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(express.static('./app/public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 consign()
     .include('app/routes')
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,4 +2,12 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('page')
     })
+    app.post('/marcar', function(req, res) {
+
+        const dados = req.body
+
+        console.log(dados)
+
+        res.redirect('https://api.whatsapp.com/send?phone=5567992943882&text=Olá me chamo '+ dados.nome +', encontrei seu numero na internet e gostaria de marcar um horario para fazer '+ dados.servico)
+    })
 }
